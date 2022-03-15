9d232c182c017f0fb1d271db2ff479de3efd7eb2
grace <grace@CHIAKI-LIV3S-HE>
2022-03-15 17:21:20 +1300

    Add project files.

---
+++ Exercise 3A/Program.cs
@@ -0,0 +1,25 @@
+﻿int age, gpa;
+
+
+
+Console.WriteLine("Enter your age: ");
+
+age = Console.ReadLine();
+
+Console.WriteLine($"Age entered was: {age}");
+
+
+
+
+
+Console.WriteLine("Enter your GPA:");
+
+gpa = Console.ReadLine();
+
+Console.WriteLine($"GPA entered was: {gpa}");
+
+
+
+Console.WriteLine("Goodbye!");
+
+
